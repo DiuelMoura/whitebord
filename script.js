462abd43b1b55dc296bb2bf5d6fd2396b25f5ee3
DiueMoura <diuelmoura@gmail.com>
2021-08-04 11:50:00 -0400

    Evento de ativação e reconhecimento de cor

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+//Initial Data
 let currentColor = 'black';
 
-document.querySelectorAll('.colorArea . color').forEach(item => {
+let screen = document.querySelector('#tela');
+let contex = screen.getContext('2d');
+//Events
+document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 });
 
+
+//Functions
 function colorClickEvent(e) {
-    let color = e.target.getA
+    let color = e.target.getAttribute('data-color');
+    console.log("COR CLICADA: ", color);
+    currentColor =  color;
+
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.add('active');
 }
